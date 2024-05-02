dea92b22e380d79237ab1d2c422fa5a0c7b3a2bc
ayalaBoyer <ayalab1515@gmail.com>
2024-05-02 12:22:40 +0300

    gh

--- a.tf
+++ a.tf
@@ -5,4 +5,8 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
+  tags = {
+    yor_name  = "example"
+    yor_trace = "607231a3-cd5c-4435-8654-0330a11d2ad3"
+  }
 }
